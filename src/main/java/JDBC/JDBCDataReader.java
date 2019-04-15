ab43d837d1eb1982ea40ec034aee7dbd06c7595c
ann <anngurjeva@gmail.com>
2019-04-16 02:09:15 +0300

    suggestion_to_update

--- src/main/java/JDBC/JDBCDataReader.java
+++ src/main/java/JDBC/JDBCDataReader.java
@@ -1,179 +1,29 @@
 package JDBC;
 
-import java.sql.*;
+import java.sql.SQLException;
 
 public class JDBCDataReader {
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost/Testtt?serverTimezone=UTC";
 
-    static final String USER = "root";
-    static final String PASS = "1234";
-
+    private void crud() {
+        try (CrudOperator crudOperator = new CrudOperator()) {
+            crudOperator.read();
+            String values1 = "1001,'Ann','Hurieva','ann@alevel.com','female','127.0.0.1'";
+            String values2 = "1002,'Hanna','Hurieva','hanna@alevel.com','female','127.0.0.1'";
+            String values3 = "1003,'Twins','Twise','2@alevel.com','male','127.0.0.1'";
+            crudOperator.insertRecord(values1);
+            crudOperator.insertRecord(values2);
+            crudOperator.insertRecord(values3);
+            crudOperator.updateRecord("Tsyguleva", 1001);
+            crudOperator.deleteRecord(1003);
+            crudOperator.updateIpByGender("8.8.8.8", "female");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error SQL !");
+        }
+    }
 
     public static void main(String[] args) {
-//        selectRecord();
-//        insertRecord();
-//        updateRecord();
-//        deleteRecord();
+        JDBCDataReader dataReader = new JDBCDataReader();
+        dataReader.crud();
     }
-
-    public static void selectRecord() {
-        Connection conn = null;
-        Statement stmt = null;
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-
-            System.out.println("Connecting to a selected database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            System.out.println("Connected database successfully...");
-
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-
-            String sql = "SELECT * FROM mock_data";
-            ResultSet rs = stmt.executeQuery(sql);
-            while (rs.next()) {
-                int ID = rs.getInt("ID");
-                String first_name = rs.getString("first_name");
-                String last_name = rs.getString("last_name");
-                String email = rs.getString("last_name");
-                String gender = rs.getString("gender");
-                String ip_address = rs.getString("ip_address");
-
-                System.out.print("ID: " + ID);
-                System.out.println(", First_name: " + first_name);
-                System.out.println(", Last_name: " + last_name);
-                System.out.println(", Email: " + email);
-                System.out.println(", Gender: " + gender);
-                System.out.println(", Ip_address: " + ip_address);
-            }
-            rs.close();
-        } catch (SQLException se) {
-            se.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (stmt != null)
-                    conn.close();
-            } catch (SQLException se) {
-            }
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
-        System.out.println("Goodbye!");
-    }
-
-    public static void insertRecord() {
-        Connection conn = null;
-        Statement stmt = null;
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-
-            System.out.println("Connecting to a selected database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            System.out.println("Connected database successfully...");
-
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-
-            String sql = "INSERT INTO mock_data VALUES (1001, 'Alex', 'sss', 'asdas@sdafa.sa', 'F', 'aaa');";
-            stmt.execute(sql);
-        } catch (SQLException se) {
-            se.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (stmt != null)
-                    conn.close();
-            } catch (SQLException se) {
-            }
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
-    }
-
-    public static void updateRecord() {
-        Connection conn = null;
-        Statement stmt = null;
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-
-            System.out.println("Connecting to a selected database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            System.out.println("Connected database successfully...");
-
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-
-            String sql = "UPDATE mock_data SET ID = '1002' WHERE ID = '1001';";
-            stmt.execute(sql);
-        } catch (SQLException se) {
-            se.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (stmt != null)
-                    conn.close();
-            } catch (SQLException se) {
-            }
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
-    }
-
-    public static void deleteRecord() {
-        Connection conn = null;
-        Statement stmt = null;
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-
-            System.out.println("Connecting to a selected database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            System.out.println("Connected database successfully...");
-
-            System.out.println("Creating statement...");
-            stmt = conn.createStatement();
-
-            String sql = "DELETE FROM mock_data WHERE ID = 1002;";
-            stmt.execute(sql);
-        } catch (SQLException se) {
-            se.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (stmt != null)
-                    conn.close();
-            } catch (SQLException se) {
-            }
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
-    }
-
 }
-
-
